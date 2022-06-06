951a16ec23a70740c649151cfb9ef02c51204747
Magda Lipka <dev@magdalipka.com>
2022-06-06 14:15:35 +0200

    add returning form answers

--- src/main/java/com/example/ankieter/controller/AnswerController.java
+++ src/main/java/com/example/ankieter/controller/AnswerController.java
@@ -3,14 +3,21 @@ package com.example.ankieter.controller;
 import com.example.ankieter.model.Answer;
 import com.example.ankieter.model.AnswerInput;
 import com.example.ankieter.model.AnswerSetInput;
+import com.example.ankieter.model.Form;
+import com.example.ankieter.model.User;
 import com.example.ankieter.repository.AnswerRepository;
+import com.example.ankieter.repository.FormRepository;
 import com.example.ankieter.repository.QuestionRepository;
+import com.example.ankieter.repository.UserRepository;
 import com.example.ankieter.utilities.Headers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
+import static java.util.stream.Collectors.groupingBy;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(methods = { RequestMethod.DELETE, RequestMethod.GET, RequestMethod.OPTIONS,
@@ -22,6 +29,10 @@ public class AnswerController {
   QuestionRepository questionRepository;
   @Autowired
   AnswerRepository answerRepository;
+  @Autowired
+  FormRepository formRepository;
+  @Autowired
+  UserRepository userRepository;
 
   @PostMapping("/forms/{form_id}/answers")
   public ResponseEntity addForm(@PathVariable(name = "form_id") String formId, @RequestHeader("Origin") String origin,
@@ -47,12 +58,23 @@ public class AnswerController {
   }
 
   @GetMapping("/forms/{form_id}/answers")
-  public ResponseEntity getFormAnswers(@RequestHeader("Origin") String origin,
+  public ResponseEntity getFormAnswers(@PathVariable(name = "form_id") String formId,
+      @RequestHeader("Origin") String origin,
       @RequestHeader("Authorization") String auth) {
 
-    // TODO
+    Form form = formRepository.getById(formId);
 
-    return ResponseEntity.ok().headers(new Headers(origin)).build();
+    if (form.getAnswersLocked()) {
+      User user = userRepository.getUserFromAuth(auth);
+      if (user == null || !user.getId().equals(form.getUserId())) {
+        return ResponseEntity.status(403).headers(new Headers(origin)).build();
+      }
+    }
+
+    List<Answer> answers = new ArrayList(
+        answerRepository.getByFormId(formId).stream().collect(groupingBy(Answer::getAnswerSetId)).values());
+
+    return ResponseEntity.ok().headers(new Headers(origin)).body(answers);
   }
 
 }
--- src/main/java/com/example/ankieter/repository/AnswerRepository.java
+++ src/main/java/com/example/ankieter/repository/AnswerRepository.java
@@ -2,9 +2,12 @@ package com.example.ankieter.repository;
 
 import com.example.ankieter.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, String> {
+  @Query(value = "select * from answers where form_id = ?1", nativeQuery = true)
+  public List<Answer> getByFormId(String formId);
 }
